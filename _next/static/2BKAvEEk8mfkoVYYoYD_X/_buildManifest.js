ba09c33ab75a7bb5c6509eefbafbf8f18c03e502
priyanka0015 <priyankasingh0015@gmail.com>
2022-05-28 19:53:07 +0530

    Updates

---
+++ _next/static/2BKAvEEk8mfkoVYYoYD_X/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-eef78eeba9cdbd9cd7bb.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
